045aa0baa30e77e8f1e181fe6d825a185a6c979d
UMAtaullin <ataullinural@gmail.com>
2026-02-12 10:38:49 +0500

    first run

---
+++ tests/test_calculator.py
@@ -0,0 +1,13 @@
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
+from src.calculator import add
+
+
+def test_add():
+    assert add(2, 3) == 5
+
+if __name__ == "__main__":
+    test_add()
+
